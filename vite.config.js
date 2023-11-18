2cbc88df949f91623565bd8e88ba5fcecc247fbb
ajor-saha <sokarama79@gmail.com>
2023-11-19 00:33:51 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Change this to the desired port
+    open: true, // Automatically open the browser when starting the development server
+  },
 })
